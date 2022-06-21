bc26922bcf0b134c26bc7b38f0f3561cb25b9385
KekoShade12 <josepaz12.32@gmail.com>
2022-06-20 23:02:42 -0300

    Comenzando interfaz grafica de la interfaz de publicidad.

---
+++ Presentacion/BackOffice/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Presentacion.BackOffice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAddBanner = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddBanner = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.btnVolver);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 449);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(171, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(629, 36);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(175, 1);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(626, 37);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Leelawadee", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(215, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Añadir Banner de publicidad";
+            // 
+            // txtAddBanner
+            // 
+            this.txtAddBanner.Location = new System.Drawing.Point(386, 187);
+            this.txtAddBanner.Name = "txtAddBanner";
+            this.txtAddBanner.Size = new System.Drawing.Size(209, 23);
+            this.txtAddBanner.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Añade la URL del banner:";
+            // 
+            // btnAddBanner
+            // 
+            this.btnAddBanner.Location = new System.Drawing.Point(604, 187);
+            this.btnAddBanner.Name = "btnAddBanner";
+            this.btnAddBanner.Size = new System.Drawing.Size(75, 23);
+            this.btnAddBanner.TabIndex = 4;
+            this.btnAddBanner.Text = "Ingresar";
+            this.btnAddBanner.UseVisualStyleBackColor = true;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVolver.ForeColor = System.Drawing.Color.White;
+            this.btnVolver.Location = new System.Drawing.Point(0, 42);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(175, 28);
+            this.btnVolver.TabIndex = 5;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddBanner);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAddBanner);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button btnVolver;
+        private Panel panel3;
+        private Label label1;
+        private TextBox txtAddBanner;
+        private Label label2;
+        private Button btnAddBanner;
+    }
+}
